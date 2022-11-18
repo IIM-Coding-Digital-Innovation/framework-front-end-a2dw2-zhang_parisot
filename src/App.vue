55ce8370ff269aee093cbc0d92ed4f4e0c292fbb
Tony Zhang <zhgtony.pro@hotmail.com>
2022-11-18 01:12:25 +0100

    :construction: working on header/search

--- src/App.vue
+++ src/App.vue
@@ -64,17 +64,3 @@ import Card from "./components/card/Card.vue";
   </section>
   
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
